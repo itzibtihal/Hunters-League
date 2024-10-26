e14126e8b6cb691f9ad2e5b27b2a4a03c6e7379a
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-10-26 14:13:16 +0200

    create spring project

---
+++ src/main/java/org/youcode/hunterleague/HunterLeagueApplication.java
@@ -0,0 +1,13 @@
+package org.youcode.hunterleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunterLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HunterLeagueApplication.class, args);
+    }
+
+}
