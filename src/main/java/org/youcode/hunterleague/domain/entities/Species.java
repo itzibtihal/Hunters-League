9ba4a314bf8222614cd41f39bbefe841f9c70a70
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-10-29 09:05:41 +0100

    add entities

---
+++ src/main/java/org/youcode/hunterleague/domain/entities/Species.java
@@ -0,0 +1,35 @@
+package org.youcode.hunterleague.domain.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.youcode.hunterleague.domain.enums.Difficulty;
+import org.youcode.hunterleague.domain.enums.SpeciesType;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Species {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private SpeciesType category;
+
+    private Double minimumWeight;
+
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
+
+    private Integer points;
+
+}
--- src/main/java/org/youcode/hunterleague/domain/entities/User.java
+++ src/main/java/org/youcode/hunterleague/domain/entities/User.java
@@ -4,66 +4,45 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.youcode.hunterleague.domain.enums.Role;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
-@Data
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
+@Setter
 @Builder
-@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "membership_number", nullable = false)
-    private String membershipNumber;
-
-    @Column(name = "first_name", nullable = false)
-    private String firstName;
-
-    @Column(name = "last_name", nullable = false)
-    private String lastName;
-
-    @Column(name = "nationality", nullable = false)
-    private String nationality;
-
-    @Column(name = "cin", nullable = false)
-    private String cin;
-
-    @Column(nullable = false)
     private String username;
 
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
     private String password;
 
-    @Column(name = "join_date", nullable = false)
-    private LocalDate joinDate;
-
-    @Column(name = "license_expiration_date", nullable = false)
-    private LocalDate licenseExpirationDate;
-
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime created_at;
-
-    @Column(name = "deleted_at")
-    private LocalDateTime deleted_at;
-
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private Role role;
 
-    @PrePersist
-    protected void onCreate() {
-        this.id = UUID.randomUUID();
-        this.created_at = LocalDateTime.now();
-    }
-    // to add in here the relations
+    private String firstName;
+
+    private String lastName;
+
+    private String cin;
+
+    private String email;
+
+    private String nationality;
+
+    private LocalDateTime joinDate;
+
+    private LocalDateTime licenseExpirationDate;
+
+    @OneToMany(mappedBy = "user")
+    private List<Participation> participations;
+
 }
+
