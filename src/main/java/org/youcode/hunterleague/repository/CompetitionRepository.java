587bb3f26fdec29876cdec73b7aad8d6d9a76ea6
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-10-31 22:33:27 +0100

    add user repository

--- src/main/java/org/youcode/hunterleague/repository/CompetitionRepository.java
+++ src/main/java/org/youcode/hunterleague/repository/CompetitionRepository.java
@@ -1,12 +1,22 @@
 package org.youcode.hunterleague.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.youcode.hunterleague.domain.Competition;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CompetitionRepository extends JpaRepository<Competition, UUID> {
+
+    Page<Competition> findByLocation(String location, Pageable pageable);
+
+    Page<Competition> findByOpenRegistration(boolean openRegistration, Pageable pageable);
+
+    List<Competition> findAllByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 
 }
