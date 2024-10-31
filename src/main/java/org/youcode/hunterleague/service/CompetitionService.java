9815b71e5cd395154577ccc3b192e4b3b45008e6
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-10-31 22:34:38 +0100

    in the last push i meant Competition repository - in this one i added the service and impl

--- src/main/java/org/youcode/hunterleague/service/CompetitionService.java
+++ src/main/java/org/youcode/hunterleague/service/CompetitionService.java
@@ -1,7 +1,26 @@
 package org.youcode.hunterleague.service;
 
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.youcode.hunterleague.domain.Competition;
 
+import java.util.UUID;
+
 public interface CompetitionService {
+
     Competition createCompetition(Competition competition);
+
+    void deleteCompetition(UUID id);
+
+    Competition updateCompetition(UUID id, @Valid Competition competitionDetails);
+
+    Competition getCompetitionDetails(UUID id);
+
+    Page<Competition> getAllCompetitions(Pageable pageable);
+
+    Page<Competition> getCompetitionsByLocation(String location, int page, int size);
+
+    Page<Competition> getCompetitionsByOpenRegistration(boolean openRegistration, int page, int size);
+
 }
