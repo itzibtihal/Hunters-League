56d61f46e11d2373bdb98886a3fdb7ac0628a599
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-11-07 15:49:26 +0100

    hunt exception

---
+++ src/main/java/org/youcode/hunterleague/exception/hunt/HuntSaveException.java
@@ -0,0 +1,7 @@
+package org.youcode.hunterleague.exception.hunt;
+
+public class HuntSaveException extends RuntimeException {
+    public HuntSaveException(String message) {
+        super(message);
+    }
+}
